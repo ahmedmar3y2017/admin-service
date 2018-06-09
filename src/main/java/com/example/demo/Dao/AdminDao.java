2cd8f416b4ef9222bc02edaf73bf445146a1fbb3
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-06-09 15:15:41 +0200

    intellij mar3y

--- src/main/java/com/example/demo/Dao/AdminDao.java
+++ src/main/java/com/example/demo/Dao/AdminDao.java
@@ -5,6 +5,9 @@ import com.example.demo.entities.Admin;
 public interface AdminDao {
     public Admin saveAdmin(Admin admin);
 
+
+
+
     public Admin updateAdmin(int id, Admin admin);
 
     public int deleteAdminById(int id);
