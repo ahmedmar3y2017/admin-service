a881ee72cc8baed6e74e842582e076996b68ccc0
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-06-10 22:19:06 +0200

    intellij mar3y

---
+++ src/main/java/com/example/demo/reportsController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import com.example.demo.ServiceImpl.BrandServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsPdfView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class reportsController {
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @Autowired
+    BrandServiceImpl brandService;
+
+    @RequestMapping(path = "/pdf", method = RequestMethod.GET)
+    public ModelAndView report() {
+
+        JasperReportsPdfView view = new JasperReportsPdfView();
+        view.setUrl("classpath:report1.jrxml");
+        view.setApplicationContext(appContext);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("datasource", brandService.getAll());
+
+        return new ModelAndView(view, params);
+    }
+
+}
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,10 +1,20 @@
+<#--<!DOCTYPE html>-->
+<#--<html>-->
+<#--<head>-->
+    <#--<meta charset="UTF-8">-->
+    <#--<title>spring-web</title>-->
+<#--</head>-->
+<#--<body>-->
+<#--<h1>You are into browser version</h1>-->
+<#--</body>-->
+<#--</html>-->
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>spring-web</title>
+    <title>Home Page</title>
 </head>
 <body>
-<h1>You are into browser version</h1>
+<a href="/pdf.html">Generate report</a>
 </body>
 </html>
