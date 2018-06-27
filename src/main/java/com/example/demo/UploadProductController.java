a7e57031cc39e9fbf575606a306ef0205248990a
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-06-27 17:12:53 +0200

    intellij mar3y

---
+++ src/main/java/com/example/demo/UploadProductController.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import com.example.demo.storage.StorageAdminService;
+import com.example.demo.storage.StorageProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Controller
+public class UploadProductController {
+
+
+    @Autowired
+    StorageProductService storageProductService;
+
+    List<String> files = new ArrayList<String>();
+
+    @PostMapping("/product/post")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            storageProductService.store(file);
+            files.add(file.getOriginalFilename());
+
+            message = "You successfully uploaded " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        } catch (Exception e) {
+            message = "FAIL to upload " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+        }
+    }
+
+    @GetMapping("/product/getallfiles")
+    public ResponseEntity<List<String>> getListFiles(Model model) {
+        List<String> fileNames = files
+                .stream().map(fileName -> MvcUriComponentsBuilder
+                        .fromMethodName(UploadController.class, "getFile", fileName).build().toString())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(fileNames);
+    }
+
+    @GetMapping("/product/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = storageProductService.loadFile(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
+}
